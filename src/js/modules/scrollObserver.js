a9c4108404ff892eb0a0d94af5753dd4dc98df5f
v.varganov <antresolrec@yandex.ru>
2024-05-28 20:59:48 +0300

    анимации по скроллу

--- src/js/modules/scrollObserver.js
+++ src/js/modules/scrollObserver.js
@@ -7,15 +7,16 @@ gsap.registerPlugin(ScrollTrigger);
 export default class ScrollObserver {
 	constructor() {
 		this.data = {
+			innerEl: '.js-scroll-observe-el-inner',
 			group: 'js-scroll-observe-group',
 			groupEl: '.js-scroll-observe-group-el',
-		}
+			groupInnerEl: 'js-scroll-observe-group-el-inner',
+		};
 
 		this.options = {
 			root: null,
 			rootMargin: '0px',
-			// процент пересечения - половина элемента
-			threshold: 1,
+			threshold: [0, 1],
 		};
 
 		this.elements = document.querySelectorAll('.js-scroll-observe-el');
@@ -36,31 +37,41 @@ export default class ScrollObserver {
 			for (const entry of entries) {
 				if (entry.isIntersecting) {
 					const el = entry.target;
+					const ratio = entry.intersectionRatio;
+					let cur = null;
 
-					if (el.classList.contains(this.data.group)) {
-						const elements = el.querySelectorAll(this.data.groupEl);
-						elements.forEach((item, index) => {
-							gsap.to(item, { 
+					if  (ratio >= 1) {
+						if (el.classList.contains(this.data.group)) {
+							const elements = el.querySelectorAll(this.data.groupEl);
+							elements.forEach((item, index) => {
+								gsap.to(item, {
+									x: 0,
+									y: 0,
+									rotate: 0,
+									opacity: 1,
+									scale: 1,
+									duration: ScrollObserver.getDuration(el),
+									delay: ScrollObserver.getDelay(el) * index,
+								});
+							});
+						} else {
+							const inner = el.querySelector(this.data.innerEl);
+							cur = inner || el;
+							gsap.to(cur, {
 								x: 0,
 								y: 0,
 								rotate: 0,
 								opacity: 1,
 								scale: 1,
-								duration: ScrollObserver.getDuration(el),
-								delay: ScrollObserver.getDelay(el) * index});
-						});
-					} else {
-						gsap.to(el, { 
-							x: 0,
-							y: 0,
-							rotate: 0,
-							opacity: 1,
-							scale: 1,
-							duration: ScrollObserver.getDuration(el),
-							delay: ScrollObserver.getDelay(el) });
+								duration: ScrollObserver.getDuration(cur),
+								delay: ScrollObserver.getDelay(cur),
+							});
+						}
+	
+						observer.unobserve(el);
 					}
 
-					observer.unobserve(el);
+
 				}
 			}
 		}, this.options);
@@ -76,13 +87,13 @@ export default class ScrollObserver {
 		}
 	}
 
-    static getDelay(el) {
-        return el.dataset.delay ? el.dataset.delay : '0';
-    }
+	static getDelay(el) {
+		return el.hasAttribute('data-delay') ? el.dataset.delay : '0';
+	}
 
-    static getDuration(el) {
-        return el.dataset.duration ? el.dataset.duration : '0.5';
-    }
+	static getDuration(el) {
+		return el.hasAttribute('data-duration') ? el.dataset.duration : '0.5';
+	}
 }
 
 export function initScrollObserver() {
